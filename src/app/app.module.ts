c9f474b493e32b92c418666b29330ad614fc5ee0
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-22 03:46:05 +0700

    Added angularfire module to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+var config = {
+  apiKey: "AIzaSyD_bpH743Uu9PcQSFi6xSOFiQ_qtuydzxU",
+  authDomain: "triage-8ae60.firebaseapp.com",
+  databaseURL: "https://triage-8ae60.firebaseio.com",
+  projectId: "triage-8ae60",
+  storageBucket: "triage-8ae60.appspot.com",
+  messagingSenderId: "617975366006"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +35,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
