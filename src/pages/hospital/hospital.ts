b4a88183bf3b6c2447215c6dc0a3a81f934c5d9c
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-23 01:14:08 +0700

    Redesign HospitalPage

--- src/pages/hospital/hospital.ts
+++ src/pages/hospital/hospital.ts
@@ -1,6 +1,8 @@
+import { CallNumber } from '@ionic-native/call-number';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 
 declare var google;
 
@@ -18,7 +20,12 @@ export class HospitalPage {
   googlePlaceService;
   googleDistanceMatrix;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    public navParams: NavParams,
+    public geolocation: Geolocation,
+    public phone: CallNumber) {
   }
 
   ionViewDidLoad() {
@@ -26,41 +33,58 @@ export class HospitalPage {
   }
 
   loadMap() {
-    this.geolocation.getCurrentPosition().then(result => {
-      let lat = result.coords.latitude
-      let lng = result.coords.longitude
-      let latLng = new google.maps.LatLng(lat, lng);
+    this.geolocation.getCurrentPosition()
+      .then(result => {
+        let lat = result.coords.latitude
+        let lng = result.coords.longitude
+        let latLng = new google.maps.LatLng(lat, lng);
 
-      let mapOptions = { center: latLng, zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP }
+        let mapOptions = { center: latLng, zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP }
 
-      this.map = new google.maps.Map(this.element.nativeElement, mapOptions)
-      this.googlePlaceService = new google.maps.places.PlacesService(this.map);
-      this.googleDistanceMatrix = new google.maps.DistanceMatrixService();
+        this.map = new google.maps.Map(this.element.nativeElement, mapOptions)
+        this.googlePlaceService = new google.maps.places.PlacesService(this.map);
+        this.googleDistanceMatrix = new google.maps.DistanceMatrixService();
 
-      let currentLocation = new google.maps.LatLng(lat, lng);
-      let nearyByRequest = { location: currentLocation, radius: '5000', type: ['hospital'] };
+        let currentLocation = new google.maps.LatLng(lat, lng);
+        let nearyByRequest = { location: currentLocation, radius: '5000', type: ['hospital'] };
 
-      this.googlePlaceService.nearbySearch(nearyByRequest, (result, status) => {
-        result.map((hospital) => {
-          let hospitalLatLng = new google.maps.LatLng(hospital.geometry.location.lat(), hospital.geometry.location.lng());
-          let distanceRequest = { origins: [currentLocation], destinations: [hospitalLatLng], travelMode: 'DRIVING', }
+        this.googlePlaceService.nearbySearch(nearyByRequest, (result, status) => {
+          result.map((hospital) => {
+            let hospitalLatLng = new google.maps.LatLng(hospital.geometry.location.lat(), hospital.geometry.location.lng());
+            let distanceRequest = { origins: [currentLocation], destinations: [hospitalLatLng], travelMode: 'DRIVING', }
 
-          this.googleDistanceMatrix.getDistanceMatrix(distanceRequest, (response) => {
-            hospital.distance = response.rows[0].elements[0].distance.text;
-            hospital.duration = response.rows[0].elements[0].duration.text;
-          });
+            this.googleDistanceMatrix.getDistanceMatrix(distanceRequest, (response) => {
+              hospital.distance = response.rows[0].elements[0].distance.text;
+              hospital.duration = response.rows[0].elements[0].duration.text;
+            });
 
-          let placeDetailRequest = { placeId: hospital.place_id };
+            let placeDetailRequest = { placeId: hospital.place_id };
 
-          this.googlePlaceService.getDetails(placeDetailRequest, (place, status) => {
-            hospital.phoneNumber = place.international_phone_number;
+            this.googlePlaceService.getDetails(placeDetailRequest, (place, status) => {
+              hospital.phoneNumber = place.international_phone_number;
+            })
           })
-        })
 
 
-        this.hospitals = result;
-      });
+          this.hospitals = result;
+        });
+      })
+      .catch(error => this.showToast(error.message))
+  }
+
+  callNumber(number: string) {
+    this.phone.callNumber(number, true)
+      .then(() => console.log('Launched dialer!'))
+      .catch(() => console.log('Error launching dialer'));
+  }
+
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
     })
+
+    toast.present();
   }
 
 }
