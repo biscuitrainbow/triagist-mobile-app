a22a113406c846d484b85a74554afb97c73677db
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-22 15:29:55 +0700

    Added google map to HospitalPage

--- src/pages/hospital/hospital.ts
+++ src/pages/hospital/hospital.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the HospitalPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-hospital',
   templateUrl: 'hospital.html',
 })
 export class HospitalPage {
+  @ViewChild('map') element;
+  map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private googleMaps: GoogleMaps) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HospitalPage');
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = this.googleMaps.create(this.element.nativeElement);
+
+    this.map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+
+      let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
+
+      let position = {
+        target: coordinates,
+        zoom: 17
+      };
+
+      this.map.animateCamera(position);
+
+    })
   }
 
 }
