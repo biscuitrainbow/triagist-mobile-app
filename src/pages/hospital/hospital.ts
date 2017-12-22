c2646f6f51d0f85d961188d60a771c257d51bfc0
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-22 20:49:47 +0700

    Added new google map api js

--- src/pages/hospital/hospital.ts
+++ src/pages/hospital/hospital.ts
@@ -11,6 +11,8 @@ declare var google;
 export class HospitalPage {
   @ViewChild('map') element;
   map: any;
+  googlePlaceService;
+  view: string = 'list'
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -29,6 +31,18 @@ export class HospitalPage {
     }
 
     this.map = new google.maps.Map(this.element.nativeElement, mapOptions)
+    this.googlePlaceService = new google.maps.places.PlacesService(this.map);
+
+    let pyrmont = new google.maps.LatLng(13.57517, 99.9433905);
+    let request = {
+      location: pyrmont,
+      radius: '5000',
+      type: ['hospital']
+    };
+
+    this.googlePlaceService.nearbySearch(request, (result, status) => console.log(result));
+
+
   }
 
 }
