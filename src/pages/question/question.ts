76ee31a4467342276346f108dc7b4177a26cb623
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-24 23:39:59 +0700

    Use question from variable instead of template hardcoding

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { firestore } from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-question',
@@ -7,30 +10,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class QuestionPage {
 
-  type: string;
-  color: string;
+  question: object;
 
-  questions: any = [
-    {
-      question: "<b>ไม่รู้สึกตัว</b> หรือ <b>ไม่หายใจ</b> หรือ <b>ไม่มีชีพจร</b>",
-      result: "2วิกฤต1"
-    },
-    {
-      question: "มีอาการ<b>บวม</b> บริเวณใบหน้า ปาก และลิ้น รวมกับมีอาการ<b>หายใจเสียงดัง</b> หรือ <b>Stridor</b>",
-      result: "2วิกฤต3"
-    },
-    {
-      question: " <b>พูดได้สั้นๆ</b> หรือ <b>หายใจมีเสียง</b> หรือ <b>ซีด</b> และ <b>มีเหงื่อท่วม</b> หรือ <b>หายใจเร็ว</b> หรือ ใช้กล้ามเนื้อ <b>ช่วยหายใจ</b>",
-      result: "2วิกฤต3"
-    }
-  ]
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.type = navParams.get('type');
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public firestore: AngularFirestore,
+    public firebaseAuth: AngularFireAuth
+  ) {
+    this.question = navParams.get('question');
   }
 
   showResult(result: string) {
-    console.log(result)
+    let uid = this.firebaseAuth.auth.currentUser.uid;
+    this.firestore
+      .collection('users')
+      .doc(uid)
+      .collection('triages')
+      .add({
+        code: result
+      })
+      .then(() => console.log("Saved successfully"))
+      .catch(error => console.log(error.message));
+
   }
 
 }
--- src/pages/triage/triage.ts
+++ src/pages/triage/triage.ts
@@ -10,9 +10,47 @@ import { App } from 'ionic-angular';
 })
 export class TriagePage {
 
-  types: any = [
-    { name: 'แพ้ยา', description: 'แพ้อาหาร , สัตว์ต่อย , ภูมิแพ้', image: 'assets/imgs/vitamins.png', color: '#B1DC76' },
-    { name: 'ได้รับสารพิษ', description: 'ยาเกินขนาด', image: 'assets/imgs/poison.png', color: '#FF85A7' }
+  questions: any = [
+    {
+      name: 'แพ้ยา',
+      description: 'แพ้อาหาร , สัตว์ต่อย , ภูมิแพ้',
+      image: 'assets/imgs/vitamins.png',
+      color: '#B1DC76',
+      questions: [
+        {
+          question: "<b>ไม่รู้สึกตัว</b> หรือ <b>ไม่หายใจ</b> หรือ <b>ไม่มีชีพจร</b>",
+          result: "2วิกฤต1"
+        },
+        {
+          question: "มีอาการ<b>บวม</b> บริเวณใบหน้า ปาก และลิ้น รวมกับมีอาการ<b>หายใจเสียงดัง</b> หรือ <b>Stridor</b>",
+          result: "2วิกฤต2"
+        },
+        {
+          question: " <b>พูดได้สั้นๆ</b> หรือ <b>หายใจมีเสียง</b> หรือ <b>ซีด</b> และ <b>มีเหงื่อท่วม</b> หรือ <b>หายใจเร็ว</b> หรือ ใช้กล้ามเนื้อ <b>ช่วยหายใจ</b>",
+          result: "2วิกฤต3"
+        }
+      ]
+    },
+    {
+      name: 'ได้รับสารพิษ',
+      description: 'ยาเกินขนาด',
+      image: 'assets/imgs/poison.png',
+      color: '#FF85A7',
+      questions: [
+        {
+          question: "<b>ไม่รู้สึกตัว</b> หรือ <b>ไม่หายใจ</b> หรือ <b>ไม่มีชีพจร</b>",
+          result: "2วิกฤต1"
+        },
+        {
+          question: "มีอาการ<b>บวม</b> บริเวณใบหน้า ปาก และลิ้น รวมกับมีอาการ<b>หายใจเสียงดัง</b> หรือ <b>Stridor</b>",
+          result: "2วิกฤต2"
+        },
+        {
+          question: " <b>พูดได้สั้นๆ</b> หรือ <b>หายใจมีเสียง</b> หรือ <b>ซีด</b> และ <b>มีเหงื่อท่วม</b> หรือ <b>หายใจเร็ว</b> หรือ ใช้กล้ามเนื้อ <b>ช่วยหายใจ</b>",
+          result: "2วิกฤต3"
+        }
+      ]
+    }
   ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public app: App) {
@@ -22,10 +60,9 @@ export class TriagePage {
     console.log('ionViewDidLoad TriagePage');
   }
 
-  startTriage(type: string, color: string) {
+  startTriage(question: object) {
     this.app.getRootNav().push(QuestionPage, {
-      type: type,
-      color: color
+      question: question
     })
   }
 
