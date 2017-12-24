7394a0c680c91cdb913d99449a259ac210809a5e
Natthapon Sricort <Natthaponsricort@gmail.com>
2017-12-25 01:07:28 +0700

    Implement ResultPage

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ResultPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-result',
   templateUrl: 'result.html',
 })
 export class ResultPage {
 
+  result;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.result = navParams.get('result');
   }
 
   ionViewDidLoad() {
