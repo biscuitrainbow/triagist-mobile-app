3907f99bf687d18a0704195fd383af1cd6eccc4d
Natthapon Sricort <natthaponsricort@gmail.com>
2018-01-06 02:39:08 +0700

    Refratoring minor code

--- src/pages/hospital/hospital.ts
+++ src/pages/hospital/hospital.ts
@@ -1,10 +1,10 @@
-import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
-import { Loading } from 'ionic-angular/components/loading/loading';
-import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { CallNumber } from '@ionic-native/call-number';
-import { Geolocation } from '@ionic-native/geolocation';
-import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+import { LoadingController } from "ionic-angular/components/loading/loading-controller";
+import { Loading } from "ionic-angular/components/loading/loading";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { CallNumber } from "@ionic-native/call-number";
+import { Geolocation } from "@ionic-native/geolocation";
+import { ToastController } from "ionic-angular/components/toast/toast-controller";
 import {
   GoogleMaps,
   GoogleMap,
@@ -13,20 +13,20 @@ import {
   CameraPosition,
   MarkerOptions,
   Marker
-} from '@ionic-native/google-maps';
+} from "@ionic-native/google-maps";
 
 declare var google;
 
 @Component({
-  selector: 'page-hospital',
-  templateUrl: 'hospital.html',
+  selector: "page-hospital",
+  templateUrl: "hospital.html"
 })
 export class HospitalPage {
-  @ViewChild('map') private mapElement;
-  @ViewChild('web_map') private webMapElement;
+  @ViewChild("map") private mapElement;
+  @ViewChild("web_map") private webMapElement;
 
   private map: any;
-  private view: string = 'map'
+  private view: string = "map";
   private hospitals: any;
 
   private loader: Loading;
@@ -41,90 +41,122 @@ export class HospitalPage {
     public navParams: NavParams,
     public geolocation: Geolocation,
     public googleMaps: GoogleMaps,
-    public phone: CallNumber) {
-  }
+    public phone: CallNumber
+  ) {}
 
   ionViewDidLoad() {
     this.loadMap();
   }
 
   loadMap() {
-    this.showLoading("Preparing map...")
-    this.geolocation.getCurrentPosition()
+    this.showLoading("Preparing map...");
+    this.geolocation
+      .getCurrentPosition()
       .then(result => {
-        let lat = result.coords.latitude
-        let lng = result.coords.longitude
+        let lat = result.coords.latitude;
+        let lng = result.coords.longitude;
 
-        this.map = this.googleMaps.create(this.mapElement.nativeElement)
+        this.map = this.googleMaps.create(this.mapElement.nativeElement);
         this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
           this.hideLoading();
 
           let webMap = new google.maps.Map(this.webMapElement.nativeElement);
-          this.googlePlaceService = new google.maps.places.PlacesService(webMap);
+          this.googlePlaceService = new google.maps.places.PlacesService(
+            webMap
+          );
           this.googleDistanceMatrix = new google.maps.DistanceMatrixService();
 
           let currentLocation = new google.maps.LatLng(lat, lng);
-          let nearyByRequest = { location: currentLocation, radius: '5000', type: ['hospital'] };
+          let nearyByRequest = {
+            location: currentLocation,
+            radius: "5000",
+            type: ["hospital"]
+          };
 
-          this.map.animateCamera({
-            'target': currentLocation,
-            'zoom': 13,
-          }, function () {
-            console.log("The animation is done");
-          });
+          this.map.animateCamera(
+            {
+              target: currentLocation,
+              zoom: 13
+            },
+            function() {
+              console.log("The animation is done");
+            }
+          );
 
-          this.googlePlaceService.nearbySearch(nearyByRequest, (result, status) => {
-            result.map((hospital) => {
+          this.googlePlaceService.nearbySearch(
+            nearyByRequest,
+            (result, status) => {
+              result.map(hospital => {
+                let hospitalLatLng = new google.maps.LatLng(
+                  hospital.geometry.location.lat(),
+                  hospital.geometry.location.lng()
+                );
+                let distanceRequest = {
+                  origins: [currentLocation],
+                  destinations: [hospitalLatLng],
+                  travelMode: "DRIVING"
+                };
 
-              let hospitalLatLng = new google.maps.LatLng(hospital.geometry.location.lat(), hospital.geometry.location.lng());
-              let distanceRequest = { origins: [currentLocation], destinations: [hospitalLatLng], travelMode: 'DRIVING', }
-
-              this.map.setMyLocationEnabled(true);
-              this.map.addMarker({
-                title: hospital.name,
-                icon: 'red',
-                animation: 'BOUNCE',
-                position: {
-                  lat: hospital.geometry.location.lat(),
-                  lng: hospital.geometry.location.lng()
-                }
-              }).then(marker => {
-                marker.on(GoogleMapsEvent.MARKER_CLICK)
-                  .subscribe(() => {
-                    // alert('clicked');
+                this.map.setMyLocationEnabled(true);
+                this.map
+                  .addMarker({
+                    title: hospital.name,
+                    icon: "red",
+                    animation: "BOUNCE",
+                    position: {
+                      lat: hospital.geometry.location.lat(),
+                      lng: hospital.geometry.location.lng()
+                    }
+                  })
+                  .then(marker => {
+                    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+                      // alert('clicked');
+                    });
                   });
+
+                this.googleDistanceMatrix.getDistanceMatrix(
+                  distanceRequest,
+                  response => {
+                    hospital.distance =
+                      response.rows[0].elements[0].distance.text;
+                    hospital.duration =
+                      response.rows[0].elements[0].duration.text;
+                  }
+                );
+
+                let placeDetailRequest = { placeId: hospital.place_id };
+
+                this.googlePlaceService.getDetails(
+                  placeDetailRequest,
+                  (place, status) => {
+                    hospital.phoneNumber = place.international_phone_number;
+                  }
+                );
               });
 
-              this.googleDistanceMatrix.getDistanceMatrix(distanceRequest, (response) => {
-                hospital.distance = response.rows[0].elements[0].distance.text;
-                hospital.duration = response.rows[0].elements[0].duration.text;
-              });
-
-              let placeDetailRequest = { placeId: hospital.place_id };
-
-              this.googlePlaceService.getDetails(placeDetailRequest, (place, status) => {
-                hospital.phoneNumber = place.international_phone_number;
-              })
-            })
-
-            this.hospitals = result;
-          });
+              this.hospitals = result;
+              console.log(this.hospitals);
+            }
+          );
         });
       })
-      .catch(error => this.showToast(error.message))
+      .catch(error => this.showToast(error.message));
   }
 
   callNumber(number: string) {
-    this.phone.callNumber(number, true)
-      .then(() => console.log('Launched dialer!'))
-      .catch(() => console.log('Error launching dialer'));
+    this.phone
+      .callNumber(number, true)
+      .then(() => console.log("Launched dialer!"))
+      .catch(() => console.log("Error launching dialer"));
   }
 
   showToast(message: string) {
-    this.toastCtrl.create({
-      message: message,
-      duration: 3000
-    }).present();
+    this.toastCtrl
+      .create({
+        message: message,
+        duration: 3000
+      })
+      .present();
   }
 
   showLoading(message: string) {
@@ -138,5 +170,4 @@ export class HospitalPage {
   hideLoading() {
     this.loader.dismiss();
   }
-
 }
