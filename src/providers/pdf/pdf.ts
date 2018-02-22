826bc1b318fffb1a904b075d16d1915e9cdee046
Natthapon Sricort <Natthaponsricort@gmail.com>
2018-02-22 15:06:23 +0700

    Remove pdf download

--- src/providers/pdf/pdf.ts
+++ src/providers/pdf/pdf.ts
@@ -64,7 +64,7 @@ export class PdfProvider {
             styles: style
           };
 
-          pdfMake.createPdf(dd).download();
+          // pdfMake.createPdf(dd).download();
           pdfMake.createPdf(dd).getBlob(blob => {
             resolve(blob);
           });
