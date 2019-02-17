cb70c215046df6aa65601243ca774bbe0f5cea85
Natthapon Sricort <natthaponsricort@gmail.com>
2019-02-18 03:04:01 +0700

    Refractor module 6,7

--- src/datas/triages/triages.ts
+++ src/datas/triages/triages.ts
@@ -3,7 +3,17 @@ import module2 from "./modules/module2";
 import module3 from "./modules/module3";
 import module4 from "./modules/module4";
 import module5 from "./modules/module5";
+import module6 from "./modules/module6";
+import module7 from "./modules/module7";
 
-const triages = [module1, module2, module3, module4, module5];
+const triages = [
+    module1, //
+    module2,
+    module3,
+    module4,
+    module5,
+    module6,
+    module7,
+];
 
 export default triages;
