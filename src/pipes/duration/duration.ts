3ea09702a8a0c42c2807c2a8ebb75be0ba9e92ca
Natthapon Sricort <natthaponsricort@gmail.com>
2019-02-19 02:33:44 +0700

    Change time format in duration pipe

--- src/pipes/duration/duration.ts
+++ src/pipes/duration/duration.ts
@@ -1,17 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from "moment";
-
+import * as moment from 'moment';
 
 @Pipe({
-  name: 'duration',
+	name: 'duration',
 })
 export class DurationPipe implements PipeTransform {
-  /**
+	/**
    * Takes a value and makes it lowercase.
    */
-  transform(value: string, ...args) {
-    // return moment.unix(Number(value)).fromNow();
-    return moment.unix(Number(value)).format("MM/DD/YYYY");
+	transform(value: string, ...args) {
+		// return moment.unix(Number(value)).fromNow();
 
-  }
+		moment.locale('th');
+
+		return moment.unix(Number(value)).add(543, 'y').format('Do MMMM YYYY HH:mm');
+	}
 }
